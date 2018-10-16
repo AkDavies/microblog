9399a2178dae3bf6e4d3287f747046622b5b294e
AkDavies <aod@andrew.cmu.edu>
2018-10-16 16:40:42 -0700

    Added follow/unfollow functionality and associated tests.

--- app/models.py
+++ app/models.py
@@ -8,6 +8,12 @@ from hashlib import md5
 def load_user(id):
     return User.query.get(int(id))
 
+followers = db.Table(
+    'followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -16,6 +22,14 @@ class User(db.Model, UserMixin):
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    followed = db.relationship(
+        'User',
+        secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'),
+        lazy='dynamic'
+    )
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -31,6 +45,24 @@ class User(db.Model, UserMixin):
         digest = md5(byte_string).hexdigest()
         gravatar_url = 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'
         return gravatar_url.format(digest,size)
+    
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+    
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+    
+    def is_following(self, user):
+        return self.followed.filter(followers.c.followed_id == user.id).count() > 0
+    
+    def followed_posts(self):
+        all_posts = Post.query.join(followers, (followers.c.followed_id == Post.user_id))
+        followed = all_posts.filter(followers.c.follower_id == self.id)
+        own_posts = Post.query.filter_by(user_id =self.id)
+        combined_posts = followed.union(own_posts)
+        return combined_posts.order_by(Post.timestamp.desc())
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -40,3 +72,4 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
